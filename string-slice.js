f80268b8039edd87890d38eeceb58a773cc5998f
dev-rahim <rahimuddin5915@gmail.com>
2022-09-20 00:35:26 +0600

    exploring array string slice()

--- string-slice.js
+++ string-slice.js
@@ -2,3 +2,6 @@ const sentence = 'a set of words that is complete in itself, typically containin
 
 const words = sentence.split(' ');
 console.log(words);
+
+const first20Cherecters = sentence.slice(0, 21);
+console.log(first20Cherecters);
