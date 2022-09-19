4fe1b060a448bb8564b13c74b028cdd78df4b41b
dev-rahim <rahimuddin5915@gmail.com>
2022-09-20 00:19:14 +0600

    exploring array string search with for of and condition and endsWith() method

--- string-search.js
+++ string-search.js
@@ -1,11 +1,11 @@
 const products = [
-    'MyWay IdeaPad, Legion, ThinkPad,MacBook ThinkBook, Yoga',
+    'MyWay IdeaPad, Legion, ThinkPad,MacBook ThinkBook, Yoga ExpertStation',
     'Elitebook, Envy, Omen, Pavilion, ZBook, Spectre, Victus, ProBook, OmniBook',
     'MyWay Alienware, MacBook G Series, Inspiron, Latitude, Precision, Vostro, XPS',
-    'MacBook, MacBook Air, MacBook Pro',
-    'MyWay,MacBook GamePower, OfficePower, ExpertStation, PowerStation, Mini-Server'
+    'MacBook, MacBook Air, MacBook Pro ExpertStation',
+    'MyWay,MacBook GamePower, OfficePower, ExpertStation, PowerStation, Mini-Server ExpertStation'
 ];
-const searcing = 'MyWay';
+const searcing = 'ExpertStation';
 const output = [];
 // for (const product of products) {
 //     if (product.toLocaleLowerCase().indexOf(searcing.toLocaleLowerCase()) != -1) {
@@ -19,8 +19,14 @@ const output = [];
 //     }
 // }
 
+// for (const product of products) {
+//     if (product.toLocaleLowerCase().startsWith(searcing.toLocaleLowerCase())) {
+//         output.push(product);
+//     }
+// }
+
 for (const product of products) {
-    if (product.toLocaleLowerCase().startsWith(searcing.toLocaleLowerCase())) {
+    if (product.toLocaleLowerCase().endsWith(searcing.toLocaleLowerCase())) {
         output.push(product);
     }
 }
