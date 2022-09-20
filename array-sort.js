3fa632b3d7e24597526dc610db74f19804ee018f
dev-rahim <rahimuddin5915@gmail.com>
2022-09-20 08:26:00 +0600

    array numbers sort and revers sort

---
+++ array-sort.js
@@ -0,0 +1,9 @@
+const numbers = [9, 5, 6, 1, 4, 3, 7, 2, 8];
+const sortedNumbers = numbers.sort();
+console.log(sortedNumbers);
+
+const numbers2 = [9, 5, 6, 96, 15, 1, 18, 4, 3, 7, 26, 2, 8, 52,];
+const sortedNumbers2 = numbers2.sort((a, b) => a - b);
+console.log(sortedNumbers2);
+const sortedRavarse = numbers2.sort((a, b) => a - b).reverse();
+console.log(sortedRavarse);
